75b01af67caa67e793298dd0d36c590a914efaa0
Rehan Malik <developerrm905@gmail.com>
2024-09-09 22:13:27 +0500

    Added aggregation pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,23 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import User from "../models/user.model.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
+import jwt from "jsonwebtoken";
+
+const generateTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    return { refreshToken, accessToken };
+  } catch (error) {
+    console.error(error);
+    throw new ApiError(500, "Something went wrong while generating tokens");
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details
@@ -29,10 +46,15 @@ const registerUser = asyncHandler(async (req, res) => {
 
   let coverImageLocalPath;
 
-  if (req.files && req.files.coverImage && Array.isArray(req.files.coverImage) && req.files.coverImage[0] && req.files.coverImage[0].path) {
+  if (
+    req.files &&
+    req.files.coverImage &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage[0] &&
+    req.files.coverImage[0].path
+  ) {
     coverImageLocalPath = req.files.coverImage[0].path;
   }
-  
 
   if (!avatarLocalPath) throw new ApiError(400, "Avatar is required 1");
 
@@ -69,4 +91,227 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, isUserCreated, "Successfully created user"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  // get data
+  const { email, password } = req.body;
+
+  // email
+  if (!email) throw new ApiError(400, "Email is required");
+
+  // find user
+  const user = await User.findOne({ email: email.toLowerCase() });
+  if (!user) throw new ApiError(404, "User does not exist");
+
+  // password check
+  const isPasswordCorrect = await user.isPasswordCorrect(password);
+  if (!isPasswordCorrect) throw new ApiError(401, "Invalid password");
+
+  // generate access and refresh token
+  const { accessToken, refreshToken } = await generateTokens(user._id);
+  // update user with refresh token
+  const loggedInUser = await User.findByIdAndUpdate(
+    user._id,
+    { refreshToken },
+    { new: true }
+  ).select("-password -refreshToken");
+
+  // send cookies
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  // return res
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "Successfully logged in"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "Successfully logged out"));
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+
+  if (!incomingRefreshToken) throw new ApiError(401, "Unauthorized Request");
+
+  const decodedToken = jwt.verify(
+    incomingRefreshToken,
+    process.env.REFRESH_TOKEN_SECRET_KEY
+  );
+
+  const user = await User.findById(decodedToken?._id);
+  if (!user) throw new ApiError(401, "Invalid Refresh Token");
+
+  if (incomingRefreshToken !== user?.refreshToken)
+    throw new ApiError(401, "Refresh token not valid");
+
+  const { refreshToken, accessToken } = await generateTokens(user?._id);
+  User.findByIdAndUpdate(
+    user._id,
+    {
+      $set: {
+        refreshToken: refreshToken,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          accessToken,
+          refreshToken,
+        },
+        "Successfully refreshed access token"
+      )
+    );
+});
+
+const changePassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user?._id);
+  if (!user) throw new ApiError(401, "Invalid user");
+
+  const isValidPassword = user.isPasswordCorrect(oldPassword);
+  if (!isValidPassword) throw new ApiError(400, "Invalid old password");
+
+  user.password = newPassword;
+  await user.save({
+    validateBeforeSave: false,
+  });
+
+  return res.json(new ApiResponse(200, {}, "Password changed successfully"));
+});
+
+const getUserDetails = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, req.user, "User details retrieved successfully")
+    );
+});
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username?.trim()) throw new ApiError(400, "Username is missing");
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        subscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        fullName: 1,
+        username: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscribersCount: 1,
+        subscribedToCount: 1,
+        isSubscribed: 1,
+      }
+    }
+  ]);
+
+  if (!channel?.length) throw new ApiError(404, "Channel does not exist");
+
+  return res.status(200)
+  .json(
+    new ApiResponse(200, channel[0], "User channel fetched succesfully")
+  );
+});
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changePassword,
+  getUserDetails,
+  getUserChannelProfile
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,24 +1,35 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import upload from "../middlewares/multer.middleware.js"
+import {
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+} from "../controllers/user.controller.js";
+import upload from "../middlewares/multer.middleware.js";
+import verifyJWT from "../middlewares/auth.middleware.js";
 
-
-const router = Router()
-
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: 'avatar',
-            maxCount: 1
-        },
-        {
-            name: 'coverImage',
-            maxCount: 1
-        }
-    ]),
-    registerUser
-)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+
+router.route("/login").post(loginUser);
+
+// Protected routes
+
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 
-export default router
+export default router;
